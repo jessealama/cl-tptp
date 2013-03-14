dca363bcdaa760ae7f0ea67639488d5116bd149a
Jesse Alama <jesse.alama@gmail.com>
2013-03-14 16:48:40 +0100

    Quick and dirty GETENV.

--- src/utils.lisp
+++ src/utils.lisp
@@ -11,3 +11,23 @@
 
 (defmethod symbolify-here ((thing string))
   (intern thing (find-package :cl-tptp)))
+
+(defgeneric getenv (environment-variable)
+  (:documentation "Get the value of ENVIRONMENT-VARIABLE from the environment.  If ENVIRONMENT-VARIABLE is undefined, return NIL."))
+
+(defmethod getenv ((variable symbol))
+  (getenv (symbol-name variable)))
+
+(defmethod getenv ((variable string))
+  (when (string= variable "")
+    (error "The empty string is not the name of an environment variable."))
+  (when (or (find #\Space variable)
+	    (find #\Newline variable)
+	    (find #\Tab variable))
+    (error "Whitespace is not allowed in (names of) environment variables."))
+  #+ccl
+  (ccl:getenv variable)
+  #+sbcl
+  (sb-posix:getenv variable)
+  #-(or ccl sbcl)
+  (error "We know how to extract values of environment variables only with CCL and SBCL."))
